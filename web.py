764db24f5761c7dcdd68c60f8c8c568f6e975f95
pooja srinivasan <58687165+poojaseenu1999@users.noreply.github.com>
2020-07-22 01:15:36 +0530

    adding studymonk link

--- web.py
+++ web.py
@@ -1,4 +1,15 @@
+#importing selenium
 from selenium import webdriver
 
+#linking the driver installed to chrome
 driver = webdriver.Chrome()
 driver.get('https://youtube.com')
+driver.get('https://www.youtube.com/results?search_query=study+monk')
+
+#linking the xpath of the searchbar to studymonk page
+# searchbox = driver.find_element_by_xpath('//*[@id="search"]')
+# searchbox.send_keys('Study Monk')
+#
+# #action when clicked on search button
+# searchbutton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
+# searchbutton.click()
